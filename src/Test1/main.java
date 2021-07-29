5e76cfc791b8949908a190da07d5a59473a15b3f
ivantsybuk <tu2-060@bk.ru>
2021-07-29 21:00:45 +0300

    Initial commit

---
+++ src/Test1/main.java
@@ -0,0 +1,9 @@
+package Test1;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
